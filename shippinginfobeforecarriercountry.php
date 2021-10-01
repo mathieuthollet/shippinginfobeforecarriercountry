18449b9873022ebcfd89503cbb9b86ed48fd5b6a
mthollet <Dg49@Ab@8d>
2021-10-01 12:04:56 +0200

    V1.0.1

--- shippinginfobeforecarriercountry.php
+++ shippinginfobeforecarriercountry.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * 2007-2020 PrestaShop
+ * 2007-2021 PrestaShop
  *
  * NOTICE OF LICENSE
  *
@@ -19,7 +19,7 @@
  * needs please refer to http://www.prestashop.com for more information.
  *
  * @author    PrestaShop SA <contact@prestashop.com>
- * @copyright 2007-2020 PrestaShop SA
+ * @copyright 2007-2021 PrestaShop SA
  * @license   http://opensource.org/licenses/afl-3.0.php  Academic Free License (AFL 3.0)
  *  International Registered Trademark & Property of PrestaShop SA
  */
@@ -39,8 +39,8 @@ class ShippingInfoBeforeCarrierCountry extends Module
     {
         $this->name = 'shippinginfobeforecarriercountry';
         $this->tab = 'checkout';
-        $this->version = '1.0.0';
-        $this->author = 'Mathieu Thollet';
+        $this->version = '1.0.1';
+        $this->author = 'AWebVision';
         $this->need_instance = 0;
         $this->bootstrap = true;
         $this->module_key = '994e0e0bd626020148aad4794cff8cec';
